ae4d47e2073d2ab2a20594e6fa334d3c6e4a4e1e
PRECIOUS AJILORE <86179834+precious010@users.noreply.github.com>
2021-11-19 14:40:03 +0000

    -I added code to write the student's name and id. -I also formatted the code that outputs the average to only show the average to two decimal places.

--- PrintGrade.java
+++ PrintGrade.java
@@ -9,6 +9,8 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 class PrintGrade {
+   //This is just formatting for the average
+   //Makes sure that it is to two decimal places
   private static final DecimalFormat df = new DecimalFormat("0.00");
 
  
@@ -19,16 +21,32 @@ class PrintGrade {
       System.out.print("Enter your name: ");
 
       String name = input.nextLine();
+      //This writes the student's name to the text file
+      myWriter.write("Student Name: "+ name);
+      //Put's the next input on a different line
+       myWriter.write(System.getProperty("line.separator"));
 
       System.out.print("Enter your student ID: ");
 
       int id = input.nextInt();
+      //Writes the student's id in the text file
+      myWriter.write("Student ID: "+ id);  
+      //puts the next input on a different line
+      myWriter.write(System.getProperty("line.separator"));
+
 
       System.out.println("Welcome, " + name.toUpperCase() + ", to the MP Mark Converter App.");
 
       System.out.println("How many courses did you take this semester? (Max num of 10 items) ");
+       
 
       int n = input.nextInt();
+      //Writes the number of courses taken in the semester 
+      //on a different line
+      myWriter.write("Number of courses taken this semester: "+ n);
+      //puts the next input on a different line
+       myWriter.write(System.getProperty("line.separator"));
+
       int[] a = new int[n];
       double sum = 0.0;
       double average = 0.0;
@@ -37,16 +55,19 @@ class PrintGrade {
         System.out.println("Enter your percent marks: ");
         a[i] = input.nextInt();
          sum = sum + a[i];
+         //puts the next input on a different line
         myWriter.write(System.getProperty("line.separator"));
+        //Writes the student's average in the text file
         myWriter.write("Your mark is: "+Integer.toString(a[i])+ "%");
       }
 
-     average = sum/(double) n; 
-      System.out.println("Your average is: " + df.format(average) + "%" );
-
+      average = sum/(double) n; 
+      String newAverage = df.format(average);
+      System.out.println("Your average is: " + newAverage + "%" );
+    
        myWriter.write(System.getProperty("line.separator"));
-      myWriter.write("Your average mark is: "+ average +"%");
-
+      myWriter.write("Your average mark is: "+ newAverage +"%");
+      myWriter.write(System.getProperty("line.separator"));
       myWriter.close();
     } catch (IOException e) {
       System.out.println("An error occurred.");
